7c501bfc124c72492bcfa1b8ef3cdd4112b43f5f
mattlaguardia <matthewlaguardia@gmail.com>
2016-05-11 09:12:08 -0700

    js-building-iterators-lab

--- myEach.js
+++ myEach.js
@@ -3,12 +3,22 @@
 function myEach(arr, callback) {
 
 //  CODE INSIDE HERE   //
-
+for (var i = 0; i < arr.length; i++) {
+	callback(arr[i], i, arr);
+	}
 }
 
 /*
  Best if you don't code out here.
  If you want to check your code, use `index.js`!
+
+1. Takes a functino as the second argument and calls that function (callback)
+2. Passes each value in the array to the callback 
+3. Passes each value in the array to the callback, even non-integers
+4. Passes each index in th array to the callback as argument 2
+5.
+
+
 */
 
 
--- myMap.js
+++ myMap.js
@@ -3,17 +3,34 @@
 function myMap(arr, callback) {
 
 //  CODE INSIDE HERE   //
+var myArray = []
+for (var i = 0; i < arr.length; i++) {
+	myArray.push(callback(arr[i], i , arr));
+	}
+	return myArray;
+} 
 
+
+/*
+ myMap.map(function(add4){
+	return add4 + 4
+	})
+return myMap;
 }
+*/
 
 /*
  Best if you don't code out here.
  If you want to check your code, use `index.js`!
 */
 
-
-
-
+/*
+for (var i = 0; i < arr.length; i++) {
+	callback(arr[i], i , arr);
+	}
+	return callback + 4;
+}
+*/
 
 
 
